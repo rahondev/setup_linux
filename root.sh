36ca66a9072f958f8fbf28b8d40eec6c609845ec
Christoph Brosch <44674265+rahondev@users.noreply.github.com>
2020-02-25 19:55:00 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -6,9 +6,9 @@ echo "Inside the root.sh script";
 echo "Installing Visual Studio Code";
 
 wget https://go.microsoft.com/fwlink/?LinkID=760868
-cp index.html/?LinkID=760868 code.deb
-rm index.html/?LinkID=760868 -f
-sudo apt install code.deb
+cp index.html\?LinkID=760868 code.deb
+rm index.html\?LinkID=760868 -f
+sudo apt install ./code.deb
 
 # Check if package git is installed
 
@@ -17,7 +17,13 @@ sudo apt-get install git
 
 # setup 'Snippet Daemon' for Visual Studio Code
 
+mkdir ~/.config/Code/User
 cd ~/.config/Code/User
 git clone https://github.com/rahondev/vsc_snippets.git
+
+mv vsc_snippets snippets
+rm vsc_snippets
+cd snippets
+sudo ./watch.sh
 
 exit 0;
